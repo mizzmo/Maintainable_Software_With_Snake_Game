85ea123594ddb30342166492038d3ba0ac1b4d66
psyts20 <psyts20@nottingham.ac.uk>
2023-12-16 01:05:44 +0000

    updating spreadsheet

--- comp2013/src/main/java/comp2013/View/SnakeMusic.java
+++ comp2013/src/main/java/comp2013/View/SnakeMusic.java
@@ -14,7 +14,6 @@ public class SnakeMusic implements IMusic
 {
 	private String M_FileName;
 	private Media M_MusicMedia;
-
 	private MediaPlayer M_MediaPlayer;
 	private double M_CurrentVolume;
 
@@ -42,10 +41,6 @@ public class SnakeMusic implements IMusic
 			this.M_MediaPlayer.setVolume(this.M_CurrentVolume);
 		}
 		else { this.M_FileName = null; }
-	}
-
-	public MediaPlayer getMediaPlayer() {
-		return M_MediaPlayer;
 	}
 
 	// Gets the File Path of the currently playing song.
